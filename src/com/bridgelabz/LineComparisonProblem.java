a78bd3e0bf9415f9deb9853e81a1beb9e4a5c4bf
Prohit <udkarprohit@gmail.com>
2023-01-17 08:44:27 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LineComparisonProblem {
+    //main method start
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
