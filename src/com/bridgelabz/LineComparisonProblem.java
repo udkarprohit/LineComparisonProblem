caf02f2c16db1da9fc0c2ef367dea95bbede41b4
Prohit <udkarprohit@gmail.com>
2023-01-17 09:46:47 +0530

    Added: Checking Two length are equal(equals method) or not

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -1,19 +1,34 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class LineComparisonProblem {
     //calculating length one.
-    static void geometryOne(int x1, int y1, int x2, int y2) {
+    static double geometryOne(int x1, int y1, int x2, int y2) {
         double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println(lengthOne);
+        return lengthOne;
+    }
+
+    //calculating length Two.
+    static double geometryTwo(int x1, int y1, int x2, int y2) {
+        double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return lengthOne;
     }
 
     //main method start
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Computation Program");
-        //passing values.
-        geometryOne(30, 40, 40, 30);
-    }
+        //passing length one values
+        Double lengthOne = geometryOne(30, 40, 40, 30);
+        System.out.println("Length one : " + lengthOne);
+        //passing length two values
+        Double lengthTwo = geometryTwo(30, 40, 40, 30);
+        System.out.println("Length two : " + lengthTwo);
+
+        //condition for checking lengthOne is equal to lengthTwo using equals method.
+        if (lengthOne.equals(lengthTwo)) {
+            System.out.println("Length one " + lengthOne + " is Equal to " + "Length Two " + lengthTwo);
+        } else {
+            System.out.println("Length one " + lengthOne + " Not Equal " + "Length Two " + lengthTwo);
+        }
+    }//main end
 }
