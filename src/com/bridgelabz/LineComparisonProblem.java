3dbebd4dc48aa97b58b5e365d044af91d8a679bc
Prohit <udkarprohit@gmail.com>
2023-01-17 08:59:46 +0530

    Added: Calculating the lenght of x1,x2 and y1,y2

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -3,9 +3,17 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparisonProblem {
+    //calculating length one.
+    static void geometryOne(int x1, int y1, int x2, int y2) {
+        double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println(lengthOne);
+    }
+
     //main method start
     public static void main(String[] args) {
-        
+
         System.out.println("Welcome to Line Comparison Computation Program");
+        //passing values.
+        geometryOne(30, 40, 40, 30);
     }
 }
